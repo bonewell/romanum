c6940c57ff2d5b558e2e7cc00ca1b1f1aa50c8b1
Konstantin Kolodiy <bonewell@yandex.ru>
2018-06-12 21:56:42 +0300

    Start refactoring of the RomanNumber class

--- romannumber.cpp
+++ romannumber.cpp
@@ -9,10 +9,17 @@ namespace {
   const std::string kIV = "IV";
   const std::string kI = "I";
   const std::string kNone = "";
+
+  std::string Slice(char digit) {
+      size_t pos = kIVXLCDM.find(digit);
+      // throw exception: digit is not supported if pos == npos
+      return kIVXLCDM.substr(0, pos);
+  }
 }
 
 RomanNumber::RomanNumber()
     : value_(),
+      digits_(kIVXLCDM),
       preDigit_(0),
       prePreDigit_(0),
       currentDigit_(0)
@@ -25,40 +32,37 @@ void RomanNumber::Append(char digit)
     prePreDigit_ = preDigit_;
     preDigit_ = currentDigit_;
     currentDigit_ = digit;
+    digits_ = Update();
 }
 
-std::string RomanNumber::AllowedDigits() const
+const std::string& RomanNumber::AllowedDigits() const
 {
+    return digits_;
+}
+
+std::string RomanNumber::Update() {
     switch (currentDigit_) {
         case 'I': return UpdateForI();
-        case 'V': return UpdateForV();
         case 'X': return UpdateForX();
-        case 'L': return UpdateForL();
         case 'C': return UpdateForC();
-        case 'D': return UpdateForD();
         case 'M': return UpdateForM();
+        case 'V':
+        case 'L':
+        case 'D': return Update5();
         default: return kIVXLCDM;
     }
+}
+
+std::string RomanNumber::Update5() const
+{
+    return Slice(preDigit_ == 0 ? currentDigit_ : preDigit_);
 }
 
 std::string RomanNumber::UpdateForI() const {
     switch (preDigit_) {
         case 'I':
-        case 'V':
-            if (prePreDigit_ == 'I')
-                return kNone;
-            else
-                return kI;
-        break;
+        case 'V': return (prePreDigit_ == 'I') ? kNone : kI;
         default: return kIVX;
-    }
-}
-
-std::string RomanNumber::UpdateForV() const
-{
-    switch (preDigit_) {
-        case 'I': return kNone;
-        default: return kI;
     }
 }
 
@@ -66,22 +70,9 @@ std::string RomanNumber::UpdateForX() const
 {
     switch (preDigit_) {
         case 'I': return kNone;
-        case 'X':
-            if (prePreDigit_ == 'X')
-                return kIV;
-            else
-                return kIVX;
-        break;
+        case 'X': return (prePreDigit_ == 'X') ? kIV : kIVX;
         case 'L': return kIVX;
         default: return kIVXLC;
-    }
-}
-
-std::string RomanNumber::UpdateForL() const
-{
-    switch (preDigit_) {
-        case 'X': return kIV;
-        default: return kIVX;
     }
 }
 
@@ -89,22 +80,9 @@ std::string RomanNumber::UpdateForC() const
 {
     switch (preDigit_) {
         case 'X': return kIV;
-        case 'C':
-            if (prePreDigit_ == 'C')
-                return kIVXL;
-            else
-                return kIVXLC;
-        break;
+        case 'C': return (prePreDigit_ == 'C') ? kIVXL : kIVXLC;
         case 'D': return kIVXLC;
         default: return kIVXLCDM;
-    }
-}
-
-std::string RomanNumber::UpdateForD() const
-{
-    switch (preDigit_) {
-        case 'C': return kIVXL;
-        default: return kIVXLC;
     }
 }
 
@@ -112,12 +90,7 @@ std::string RomanNumber::UpdateForM() const
 {
     switch (preDigit_) {
         case 'C': return kIVXL;
-        case 'M':
-            if (prePreDigit_ == 'M')
-                return kIVXLCD;
-            else
-                return kIVXLCDM;
-        break;
+        case 'M': return (prePreDigit_ == 'M') ? kIVXLCD : kIVXLCDM;
         default: return kIVXLCDM;
     }
 }
--- romannumber.h
+++ romannumber.h
@@ -8,18 +8,18 @@ class RomanNumber
 public:
     RomanNumber();
     void Append(char digit);
-    std::string AllowedDigits() const;
+    const std::string& AllowedDigits() const;
 
 private:
+    std::string Update();
+    std::string Update5() const;
     std::string UpdateForI() const;
-    std::string UpdateForV() const;
     std::string UpdateForX() const;
-    std::string UpdateForL() const;
     std::string UpdateForC() const;
-    std::string UpdateForD() const;
     std::string UpdateForM() const;
 
     std::string value_;
+    std::string digits_;
     char preDigit_;
     char prePreDigit_;
     char currentDigit_;
